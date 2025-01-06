3405b432dfd79ef8bf5654c8a5835c2f1db94099
Yugeshwaran-gm <yugeshwarangm@gmail.com>
2025-01-06 13:38:18 +0530

    changed yugesh.jsx

--- src/userManagement/CreateUser.jsx
+++ src/userManagement/CreateUser.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import React  from 'react'
-import axios from 'axios'
+import axios from 'axios';
 import { useNavigate } from "react-router-dom"
 
 const CreateUser = () => {
